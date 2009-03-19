e746f7a6cf4c54caed7f95981a61b61cf48d6a6c
Tomasz Godniak <tomasz.godniak@u2i.com>
2009-03-19 21:01:58 +0100

    Added default params. Added addHandlers function. Added calculation of width or height of juggler

--- proto_juggler.js
+++ proto_juggler.js
@@ -1,10 +1,21 @@
 var Juggler = Class.create({
+	// Default params
+	defaults: $H({
+		prevId:      'prev_btn',
+		nextId:      'next_btn',
+		itemsToMove: 1,
+		itemsToShow: 1,
+		itemSize:    100,
+		vertical:    true
+	    }),
 	initialize: function(element, options) {
-	    this.element = $(element);
-	    this.id      = (element.id == null || element.id == '') ? this.generateRandomId() : element.id;
-	    this.params  = options    || {};
-	    Object.extend(this, options);
-	}
+	    this.container = $(element);
+	    if(!this.container) { return false; }
+	    // this.id      = (element.id == null || element.id == '') ? this.generateRandomId() : element.id;
+	    this.params  = this.defaults.merge(options);
+	    Object.extend(this, this.params);
+	    this.init();
+	},
     });
 
 Juggler.fn = Juggler.prototype;
@@ -18,4 +29,35 @@ Juggler.fn.generateRandomId = function() {
 	randomString += chars.substring(rnum, rnum + 1);
     }
     return randomString;
+};
+
+Juggler.fn.init = function() {
+    this.juggler = $(this.container.down('ul'));
+    this.juggler.setStyle('position: relative; left: 0px; top: 0px;');
+    this.items = this.juggler.childElements();
+    this.container.setStyle('overflow:hidden');
+    this.params.set('itemsCount', this.items.size());
+    if(this.params.get('vertical')) {
+	this.items.each(function(el) { el.setStyle('float:left;') });
+	this.params.set('itemSize', this.items.first().getWidth());
+	this.juggler.setStyle('width:' + (this.params.get('itemSize')*this.params.get('itemsCount')) + 'px;');
+	this.container.setStyle('width:' + (this.params.get('itemSize')*this.params.get('itemsToShow')) + 'px;');
+    } else {
+	this.params.set('itemSize', this.items.first().getHeight());
+	this.container.setStyle('height:' + (this.params.get('itemSize')*this.params.get('itemsCount')) + 'px;');
+	this.juggler.setStyle('height:' + (this.params.get('itemSize')*this.params.get('itemsToShow')) + 'px;');
+    }
+    this.prevBtn = $(this.params.get('prevId'));
+    this.nextBtn = $(this.params.get('nextId'));
+};
+
+Juggler.fn.addHandlers = function() {
+    if(this.get('itemsCount') > this.params.get('itemsToShow')) {
+	this.nextBtn.onclick = function() {this.nextItem();return false;}.bind(this);
+	this.prevBtn.onclick = function() {this.prevItem();return false;}.bind(this);
+    } else {
+	this.prevBtn.onclick = function() {return false;};
+	this.nextBtn.onclick = function() {return false;};
+	this.nextBtn.addClassName('next-inactive');
+    }
 };
